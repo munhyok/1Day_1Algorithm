aa7e49b30f9115982ab1907a86c07311d963f94e
Munhyeok <mhkang990510@naver.com>
2024-04-05 19:30:51 +0900

    Time: 412 ms (77.85%), Space: 32.1 MB (32.19%) - LeetHub

---
+++ LeetCode/0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,12 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        hashset = set()
+        
+        for i in nums:
+            if i in hashset:
+                return True
+            hashset.add(i)
+        return False
+        
+        #print(set(nums))
